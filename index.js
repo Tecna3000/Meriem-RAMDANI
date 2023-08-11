5b2607e821e8f4d827d8240fb6a4d1c68dc6b0d4
Tecna3000 <ramdani.meriem2001@gmail.com>
2023-08-11 21:45:50 +0200

    site done

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// ---
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
 const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
@@ -22,3 +21,26 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+const video1 = document.getElementById('video1');
+const video2 = document.getElementById('video2');
+const nextButton = document.getElementById('nextButton');
+
+nextButton.addEventListener('click', () => {
+  if (video1.style.display === 'block') {
+    video1.style.display = 'none';
+    video2.style.display = 'block';
+  } else {
+    video1.style.display = 'block';
+    video2.style.display = 'none';
+  }
+});
+const header = document.querySelector('.header');
+
+const scrollUpAmount = 60; 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > scrollUpAmount) {
+    header.classList.add('header-scroll');
+  } else {
+    header.classList.remove('header-scroll');
+  }
+});
